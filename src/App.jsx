d9dce04b7456c79174421bf52f7383b329a4c203
Lorenzo Cazzato <120551403+lorenzocazzato@users.noreply.github.com>
2023-09-04 10:54:12 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hello from "./Class/Hello";
+import Message from "./Class/Message";
+
+function App() {
+  return (
+    <>
+      <Hello />
+      <Message />
+    </>
+  );
+}
+
+export default App;
