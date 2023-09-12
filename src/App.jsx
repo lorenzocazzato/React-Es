52468fc3b2adac60aff24208a16c6a1838a19d6e
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-09-12 09:01:40 +0200

    es17

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css";
-import Hello from "./Class/Hello";
-import Message from "./Class/Message";
-import Welcome from "./Class/Welcome";
-import Counter from "./Class/counter";
 
-import ClickCounter from "./Class/clickcounter";
+import ClickTracker from "./Class/clicktracker";
 
 function App() {
   const jsxExpression = <strong>Lorenzo</strong>;
@@ -14,7 +10,7 @@ function App() {
 
   return (
     <>
-      <ClickCounter />
+      <ClickTracker />
     </>
   );
 }
