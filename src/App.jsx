8db903ba9cee40ef31f1f95a260ea6a0705992d8
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-09-27 10:57:36 +0200

    es47

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Counter } from "./Functions/counter";
 import { ShowGitUser } from "./Functions/showgituser";
+import { GithubUserList } from "./Functions/githubuserlist";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Link to="/">Home</Link>|<Link to="/counter">Counter</Link>|
-        <Link to="users/lorenzocazzato">Vai al nome</Link>
+        <Link to="/users">Lista User</Link>
         <Routes>
           <Route path="/counter" element={<Counter />} />
-          <Route
-            path="/"
-            element={<Link to="users/lorenzocazzato">GitUser</Link>}
-          />
-
-          <Route path="users/:username" element={<ShowGitUser />} />
+          <Route path="/" element={<p>HOME</p>} />
+          <Route path="/users" element={<GithubUserList />}>
+            <Route index element={<p>Add a user and select it</p>} />
+            <Route path=":username" element={<ShowGitUser />} />
+          </Route>
           <Route path="*" element={<p>Not found</p>} />
         </Routes>
       </div>
